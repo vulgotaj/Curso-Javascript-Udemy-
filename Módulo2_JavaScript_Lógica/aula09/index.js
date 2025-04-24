2f595ed14bfb1520f2f844fb18ad20cf12088d69
Felipe Tajima <felipebtajima05@gmail.com>
2025-04-24 20:17:24 -0300

    Atribuição via Desestruturação de Arrays e Objetos, For Clássico

---
+++ Módulo2_JavaScript_Lógica/aula09/index.js
@@ -0,0 +1,13 @@
+/*
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [um, , tres, , cinco] = numeros;
+console.log(um, tres, cinco)
+*/
+
+const numeros = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+const [lista1, lista2, lista3] = numeros;
+console.log(lista3[2]);
