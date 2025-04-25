fad2bc8203d0bd618db235ff31d093ab1525b6da
Felipe Tajima <felipebtajima05@gmail.com>
2025-04-24 22:27:30 -0300

    exercicio com for

--- Módulo2_JavaScript_Lógica/aula12 (exercicio)/assets/js/main.js
+++ Módulo2_JavaScript_Lógica/aula12 (exercicio)/assets/js/main.js
@@ -1,80 +1,25 @@
-const form = document.querySelector('form');
-const button = form.querySelector('button');
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'}
+];
 
+const container = document.querySelector('.container')
+const div = document.createElement('div');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const inputPeso = form.querySelector('#input-peso');
-    const inputAltura = form.querySelector('#input-altura');
-
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
-
-    if (!peso) {
-        setResultado('Peso inválido!', false);
-        return;
-    }
-
-    if (!altura) {
-        setResultado('Altura inválida', false);
-        return;
-    }
-
-    const imc = getImc(peso, altura);
-    const nivelImc = getNivelImc(imc);
-
-    const msg = `Seu IMC é ${imc} (${nivelImc})`
-
-});
-
-function getNivelImc (imc) {
-    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
-
-    if (imc >= 39.9) {
-        return nivel[5];
-    } 
-    
-    if (imc >= 34.9) {
-        return nivel[4];
-    }
-    
-    if (imc >= 29.9) {
-        return nivel[3];
-    }
-    
-    if (imc >= 24.9) {
-        return nivel[2];
-    }
-    
-    if (imc >= 18.5) {
-        return nivel[1];
-    }
-    
-    if (imc < 18.5) {
-        return nivel[0];
-    }
+for (let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
 }
 
-function getImc(peso, altura) {
-    const imc = peso / altura ** 2;
-    return imc.toFixed(2);
+container.appendChild(div);
+/*
+for (let i = 0; i < elementos.length; i++) {
+    const {tag, texto} = elementos[i] 
+    container.innerHTML +=
+    `<${tag}>${texto}</${tag}`
 }
-
-function criaP () {
-    const p = document.createElement('p');
-    return p;
-}
-
-function setResultado(msg, isValid) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-    const p = criaP();
-
-    if (isValid) {
-        p.classList.add('paragrafo-resultado');
-    } else {
-        p.classList.add('bad');
-    }
-    p.innerHTML = msg
-    resultado.appendChild(p)
-}
+*/
