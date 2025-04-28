5105e3eeee32b00b499a6fb70e82e23d8959cf53
Felipe Tajima <felipebtajima05@gmail.com>
2025-04-28 18:01:25 -0300

    Exercício número máximo

---
+++ Módulo2_JavaScript_Lógica/aula17/index.js
@@ -0,0 +1,14 @@
+const numeros = [1,2,3,4,5,6,7,8,9];
+
+for (let numero of numeros) {
+
+    if (numero === 2 || numero === 5) {
+        continue;
+    }
+
+    if (numero === 7) {
+        break;
+    }
+
+    console.log(numero);
+}
---
+++ Módulo2_JavaScript_Lógica/aula18 (exercicio)/index.js
@@ -0,0 +1,12 @@
+const max2 = (x, y) => x > y ? x : y;
+
+console.log(max2(10,2));
+
+/*
+function biggerNum (num1, num2) {
+    const NumArray = [num1, num2];
+    console.log(Math.max(...NumArray))
+}
+
+biggerNum(5, 10)
+*/
