7b7bfd73c074b80c89a3fa60cf7a0c92371410e3
Felipe Tajima <felipebtajima05@gmail.com>
2025-05-13 18:49:56 -0300

    Splice e concatenação de arrays

---
+++ Módulo4_JavaScript_Arrays(Avançado)/aula02/index.js
@@ -0,0 +1,9 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(índice, delete, elem1, elem2, elem3);
+// pop
+//const removidos = nomes.splice(3, 1, 'Luiz', 'Otávio');
+//console.log(nomes, removidos);
+nomes.splice(0, 0, 'Luiz');
+
+console.log(nomes);
